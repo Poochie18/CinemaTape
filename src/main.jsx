957f74c952cad857c1cb3103a058b0c0f66265e9
Mykola Vasylchenko <3030cocacola@gmail.com>
2026-01-14 16:28:13 +0200

    v2.2

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './AppRouter.jsx'
 import './index.css'
 
+// GitHub Pages SPA redirect handling
+const redirect = sessionStorage.redirect;
+delete sessionStorage.redirect;
+if (redirect && redirect !== location.href) {
+  history.replaceState(null, null, redirect);
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
